f87953e656dd11edcdf7bc682683eff2497e0f31
Kopano2303 <ST10491894@rcconnect.edu.za>
2026-08-14 14:00:48 +0200

    Student Class

--- ConsoleApp1/Student.cs
+++ ConsoleApp1/Student.cs
@@ -8,6 +8,15 @@ namespace ConsoleApp1
 {
     public class Student
     {
-        
+        public string StudentName { get; set; }
+        public string StudentNumber { get; set; }
+        public double Marks { get; set; }
+
+        public Student(string studentName, string studentNumber, double marks)
+        {
+            StudentName = studentName;
+            StudentNumber = studentNumber;
+            Marks = marks;
+        }
     }
 }
